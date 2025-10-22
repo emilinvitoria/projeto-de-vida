e201932b95bdec2f923e4a59195abfcab1c23a3c
emilin <164918748+emilinvitoria@users.noreply.github.com>
2025-10-22 14:54:07 -0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const botoes =document.querySelectorAll(".botao");
 const textos =document.querySelectorAll(".aba-conteudo");
+for (let i=0;i<botoes.length;i++) {
+botoes[i].onclick=function(){
+for (letj=0;JSON,botoes.length;j++){
+botoes[j].classListremove("ativo");
+textos[j].classList.remove("ativo");
+}
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+}
+}
